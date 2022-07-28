7d71fe8651a17178bb8f23eaeef68b010ae0e175
DFarias8791 <dfarias8791@gmail.com>
2022-07-28 07:38:23 -0300

    Registro de productos

--- app.py
+++ app.py
@@ -1,5 +1,55 @@
+
+from flask import jsonify
 from pymongo import MongoClient 
+from datetime import datetime
+import traceback
 
+
+# conexión a base de datos
 MONGO_URI = 'mongodb://localhost'
-
 client = MongoClient(MONGO_URI)
+
+# iniciamos la base de datos
+db = client['store']
+
+# inicial la coleccion stock
+stock = db['products']
+
+
+def insertNewProduct(stock):
+    '''
+    Función para ingresar productos al stock de inventario
+    '''
+    now = datetime.now()
+
+    insert = 's'
+    while insert == 's':
+        # ingreso del producto
+        admissionDate = datetime(now.year, now.month, now.day, 0, 0)
+        admissionDate.strftime('%Y/%m/%d')
+        typeProduct = input("Naturaleza del producto: ").upper()
+        name = input("Marca del producto: ").upper()
+        productExpiration = input("Indique fecha de vencimiento: ")
+        amount = int(input("Indique cantidad de producto ingresado: "))
+        price = float(input("Ingrese precio del producto: "))
+        print(admissionDate, typeProduct, name, productExpiration, amount, price)
+        # registro en DB
+        try:
+            stock.insert_one({
+                'admissionDate': admissionDate,
+                'typeProduct': typeProduct,
+                'name': name,
+                'productExpiration': productExpiration,
+                'amount': amount,
+                'price': price
+            })
+        except:
+            # error
+            return jsonify({'trace': traceback.format_exc})
+
+        insert = input("¿Desea ingresar otro producto? s/n: ")
+
+
+if __name__ == '__main__':
+
+    insertNewProduct(stock)
